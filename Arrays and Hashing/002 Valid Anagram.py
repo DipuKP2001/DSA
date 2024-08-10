9e1e4de6305cb3d6f53d884f9f5ec4d1225bdab7
DipuKP2001 <dipukp602@gmail.com>
2024-08-10 22:33:32 +0530

    Two Sum

--- Arrays and Hashing/002 Valid Anagram.py
+++ Arrays and Hashing/002 Valid Anagram.py
@@ -1,7 +1,7 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
-        return sorted(s) == sorted(t) #- Best mtd
+        return sorted(s) == sorted(t) #- Best mtd  
 
         # if (len(s) != len(t)):
         #     return False
